4450bcaaec44a4127f416037d48953e48ec5d715
Vincent Velociter <vincent.velociter@gmail.com>
2022-06-06 11:34:56 +0200

    Don't export readFen

--- lib/chessground.dart
+++ lib/chessground.dart
@@ -2,6 +2,5 @@ library chessground;
 
 export 'src/widgets/board.dart';
 export 'src/models.dart';
-export 'src/fen.dart';
 export 'src/settings.dart';
 export 'src/theme.dart';
--- test/fen_test.dart
+++ test/fen_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:chessground/chessground.dart';
+import 'package:chessground/src/fen.dart';
 
 void main() {
   test('read fen', () {
