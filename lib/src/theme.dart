f8280c680c40b546cda8345256cd186ceda48b28
Benjamin Lehmann <bclehmann@yahoo.ca>
2023-02-12 22:06:39 -0700

    Adds backgrounds and themes for new board images

--- lib/src/theme.dart
+++ lib/src/theme.dart
@@ -66,10 +66,200 @@ class BoardTheme {
     validPremoves: Color(0x60203085),
   );
 
+  static const blue2 = BoardTheme(
+    background: Background.blue2,
+    whiteCoordBackground: Background.blue2WhiteCoords,
+    blackCoordBackground: Background.blue2BlackCoords,
+    lastMove: Color(0x809cc700),
+    selected: Color(0x8014551e),
+    validMoves: Color(0x6014551e),
+    validPremoves: Color(0x60203085),
+  );
+
+  static const blue3 = BoardTheme(
+    background: Background.blue3,
+    whiteCoordBackground: Background.blue3WhiteCoords,
+    blackCoordBackground: Background.blue3BlackCoords,
+    lastMove: Color(0x809cc700),
+    selected: Color(0x8014551e),
+    validMoves: Color(0x6014551e),
+    validPremoves: Color(0x60203085),
+  );
+
+  static const blueMarble = BoardTheme(
+    background: Background.blueMarble,
+    whiteCoordBackground: Background.blueMarbleWhiteCoords,
+    blackCoordBackground: Background.blueMarbleBlackCoords,
+    lastMove: Color(0x809cc700),
+    selected: Color(0x8014551e),
+    validMoves: Color(0x6014551e),
+    validPremoves: Color(0x60203085),
+  );
+
+  static const canvas2 = BoardTheme(
+    background: Background.canvas2,
+    whiteCoordBackground: Background.canvas2WhiteCoords,
+    blackCoordBackground: Background.canvas2BlackCoords,
+    lastMove: Color(0x809cc700),
+    selected: Color(0x8014551e),
+    validMoves: Color(0x6014551e),
+    validPremoves: Color(0x60203085),
+  );
+
+  static const greenPlastic = BoardTheme(
+    background: Background.greenPlastic,
+    whiteCoordBackground: Background.greenPlasticWhiteCoords,
+    blackCoordBackground: Background.greenPlasticBlackCoords,
+    lastMove: Color(0x809cc700),
+    selected: Color(0x8014551e),
+    validMoves: Color(0x6014551e),
+    validPremoves: Color(0x60203085),
+  );
+
+  static const grey = BoardTheme(
+    background: Background.grey,
+    whiteCoordBackground: Background.greyWhiteCoords,
+    blackCoordBackground: Background.greyBlackCoords,
+    lastMove: Color(0x809cc700),
+    selected: Color(0x8014551e),
+    validMoves: Color(0x6014551e),
+    validPremoves: Color(0x60203085),
+  );
+
   static const horsey = BoardTheme(
     background: Background.horsey,
     whiteCoordBackground: Background.horseyWhiteCoords,
     blackCoordBackground: Background.horseyBlackCoords,
+    lastMove: Color(0x809cc700),
+    selected: Color(0x8014551e),
+    validMoves: Color(0x6014551e),
+    validPremoves: Color(0x60203085),
+  );
+
+  static const leather = BoardTheme(
+    background: Background.leather,
+    whiteCoordBackground: Background.leatherWhiteCoords,
+    blackCoordBackground: Background.leatherBlackCoords,
+    lastMove: Color(0x809cc700),
+    selected: Color(0x8014551e),
+    validMoves: Color(0x6014551e),
+    validPremoves: Color(0x60203085),
+  );
+
+  static const maple = BoardTheme(
+    background: Background.maple,
+    whiteCoordBackground: Background.mapleWhiteCoords,
+    blackCoordBackground: Background.mapleBlackCoords,
+    lastMove: Color(0x809cc700),
+    selected: Color(0x8014551e),
+    validMoves: Color(0x6014551e),
+    validPremoves: Color(0x60203085),
+  );
+
+  static const maple2 = BoardTheme(
+    background: Background.maple2,
+    whiteCoordBackground: Background.maple2WhiteCoords,
+    blackCoordBackground: Background.maple2BlackCoords,
+    lastMove: Color(0x809cc700),
+    selected: Color(0x8014551e),
+    validMoves: Color(0x6014551e),
+    validPremoves: Color(0x60203085),
+  );
+
+  static const marble = BoardTheme(
+    background: Background.marble,
+    whiteCoordBackground: Background.marbleWhiteCoords,
+    blackCoordBackground: Background.marbleBlackCoords,
+    lastMove: Color(0x809cc700),
+    selected: Color(0x8014551e),
+    validMoves: Color(0x6014551e),
+    validPremoves: Color(0x60203085),
+  );
+
+  static const metal = BoardTheme(
+    background: Background.metal,
+    whiteCoordBackground: Background.metalWhiteCoords,
+    blackCoordBackground: Background.metalBlackCoords,
+    lastMove: Color(0x809cc700),
+    selected: Color(0x8014551e),
+    validMoves: Color(0x6014551e),
+    validPremoves: Color(0x60203085),
+  );
+
+  static const ncf = BoardTheme(
+    background: Background.ncf,
+    whiteCoordBackground: Background.ncfWhiteCoords,
+    blackCoordBackground: Background.ncfBlackCoords,
+    lastMove: Color(0x809cc700),
+    selected: Color(0x8014551e),
+    validMoves: Color(0x6014551e),
+    validPremoves: Color(0x60203085),
+  );
+
+  static const newspaper = BoardTheme(
+    background: Background.newspaper,
+    whiteCoordBackground: Background.newspaperWhiteCoords,
+    blackCoordBackground: Background.newspaperBlackCoords,
+    lastMove: Color(0x809cc700),
+    selected: Color(0x8014551e),
+    validMoves: Color(0x6014551e),
+    validPremoves: Color(0x60203085),
+  );
+
+  static const olive = BoardTheme(
+    background: Background.olive,
+    whiteCoordBackground: Background.oliveWhiteCoords,
+    blackCoordBackground: Background.oliveBlackCoords,
+    lastMove: Color(0x809cc700),
+    selected: Color(0x8014551e),
+    validMoves: Color(0x6014551e),
+    validPremoves: Color(0x60203085),
+  );
+
+  static const pinkPyramid = BoardTheme(
+    background: Background.pinkPyramid,
+    whiteCoordBackground: Background.pinkPyramidWhiteCoords,
+    blackCoordBackground: Background.pinkPyramidBlackCoords,
+    lastMove: Color(0x809cc700),
+    selected: Color(0x8014551e),
+    validMoves: Color(0x6014551e),
+    validPremoves: Color(0x60203085),
+  );
+
+  static const purpleDiag = BoardTheme(
+    background: Background.purpleDiag,
+    whiteCoordBackground: Background.purpleDiagWhiteCoords,
+    blackCoordBackground: Background.purpleDiagBlackCoords,
+    lastMove: Color(0x809cc700),
+    selected: Color(0x8014551e),
+    validMoves: Color(0x6014551e),
+    validPremoves: Color(0x60203085),
+  );
+
+  static const wood = BoardTheme(
+    background: Background.wood,
+    whiteCoordBackground: Background.woodWhiteCoords,
+    blackCoordBackground: Background.woodBlackCoords,
+    lastMove: Color(0x809cc700),
+    selected: Color(0x8014551e),
+    validMoves: Color(0x6014551e),
+    validPremoves: Color(0x60203085),
+  );
+
+  static const wood2 = BoardTheme(
+    background: Background.wood2,
+    whiteCoordBackground: Background.wood2WhiteCoords,
+    blackCoordBackground: Background.wood2BlackCoords,
+    lastMove: Color(0x809cc700),
+    selected: Color(0x8014551e),
+    validMoves: Color(0x6014551e),
+    validPremoves: Color(0x60203085),
+  );
+
+  static const wood3 = BoardTheme(
+    background: Background.wood3,
+    whiteCoordBackground: Background.wood3WhiteCoords,
+    blackCoordBackground: Background.wood3BlackCoords,
     lastMove: Color(0x809cc700),
     selected: Color(0x8014551e),
     validMoves: Color(0x6014551e),
