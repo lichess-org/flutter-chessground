4216898d670a99fa896a8ff17486127d3e049e67
Vincent Velociter <vincent.velociter@gmail.com>
2024-10-11 08:50:54 +0200

    Update doc of image cache

--- lib/src/chessground.dart
+++ lib/src/chessground.dart
@@ -5,7 +5,7 @@ import 'images.dart';
 // ignore: avoid_classes_with_only_static_members
 /// Class that provides static access to shared instances used by the package.
 class Chessground {
-  /// Access to the shared instance of piece images cache.
+  /// Access to the shared instance of [PieceImages].
   ///
   /// By default, the cache is configured to retrieve images using the [rootBundle].
   /// If you need to use a different [AssetBundle], you can create a new instance of [PieceImages].
--- lib/src/images.dart
+++ lib/src/images.dart
@@ -1,15 +1,47 @@
 import 'dart:async';
-import 'dart:ui';
+import 'dart:ui' as ui;
 
-import 'package:flutter/painting.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+
+import 'chessground.dart';
+import 'widgets/piece.dart';
 
 // Inspired by Flame Engine image cache:
 // https://github.com/flame-engine/flame/blob/main/packages/flame/lib/src/cache/images.dart
 
 /// A cache for chess piece images.
 ///
-/// This class is used to load and cache images for chess pieces.
+/// A static instance of this class is available as [Chessground.images] and is used
+/// by the [PieceWidget] to get images for chess pieces.
+///
+/// This is useful to avoid using the global flutter image cache and the standard
+/// [Image] widget which can be unpredictable, and can cause images to blink.
+///
+/// The images should be preloaded into the cache using the [load] method before
+/// they are used in the [PieceWidget]. This will ensure that the images are
+/// available when the [PieceWidget] is built.
+/// The name of the cache entry must be the same as the [AssetImage.assetName] in
+/// order to be used by the [PieceWidget].
+///
+/// Example:
+/// ```dart
+/// // in main.dart
+/// for (final asset in pieceSet.assets.values) {
+///      final key = await asset
+///          .obtainKey(ImageConfiguration(devicePixelRatio: devicePixelRatio));
+///      await Chessground.images.load(asset.assetName, key.name);
+/// }
+/// ```
+///
+/// Using the cache is optional, and the [PieceWidget] will load images directly
+/// from the [AssetBundle] if the cache is not used.
+///
+/// When the pieces are cached in memory, the [PieceWidget] class will display
+/// the piece image using the [RawImage] widget.
+///
+/// This is the responsibility of the user to dispose of the cache when it is no
+/// longer needed, or when changing the piece set, using the [clearCache] method.
 class PieceImages {
   PieceImages(this.bundle);
 
@@ -22,7 +54,7 @@ class PieceImages {
   ///
   /// No error is raised if the image [name] is not present in the cache.
   ///
-  /// This calls [Image.dispose], so make sure that you don't use the previously
+  /// This calls [ui.Image.dispose], so make sure that you don't use the previously
   /// cached image once it is cleared (removed) from the cache.
   void clear(String name) {
     final removedAsset = _assets.remove(name);
@@ -31,7 +63,7 @@ class PieceImages {
 
   /// Removes all cached images.
   ///
-  /// This calls [Image.dispose] for all images in the cache, so make sure that
+  /// This calls [ui.Image.dispose] for all images in the cache, so make sure that
   /// you don't use any of the previously cached images once [clearCache] has
   /// been called.
   void clearCache() {
@@ -45,13 +77,16 @@ class PieceImages {
   /// doesn't need to be explicitly disposed.
   ///
   /// If you want to retain the image even after you remove it from the cache,
-  /// then you can call `Image.clone()` on it.
-  Image? fromCache(String name) {
+  /// then you can call [ui.Image.clone] on it.
+  ui.Image? fromCache(String name) {
     return _assets[name]?.image;
   }
 
   /// Loads the specified image with [fileName] into the cache with the given [name].
-  Future<Image> load(String name, String fileName) {
+  ///
+  /// The [name] must be the [AssetImage.assetName] of the piece image in order to
+  /// be used by the [PieceWidget].
+  Future<ui.Image> load(String name, String fileName) {
     return (_assets[name] ??= _ImageAsset.future(_fetchToMemory(fileName)))
         .retrieveAsync();
   }
@@ -62,7 +97,7 @@ class PieceImages {
   /// Returns the list of keys in the cache.
   List<String> get keys => _assets.keys.toList();
 
-  String? findKeyForImage(Image image) {
+  String? findKeyForImage(ui.Image image) {
     return _assets.keys.firstWhere(
       (k) => _assets[k]?.image?.isCloneOf(image) ?? false,
     );
@@ -73,31 +108,31 @@ class PieceImages {
     return Future.wait(_assets.values.map((asset) => asset.retrieveAsync()));
   }
 
-  Future<Image> _fetchToMemory(String name) async {
+  Future<ui.Image> _fetchToMemory(String name) async {
     final data = await bundle.load(name);
     final bytes = Uint8List.view(data.buffer);
     return decodeImageFromList(bytes);
   }
 }
 
-/// Individual entry in the [Images] cache.
+/// Individual entry in the [PieceImages] cache.
 ///
-/// This class owns the [Image] object, which can be disposed of using the
+/// This class owns the [ui.Image] object, which can be disposed of using the
 /// [dispose] method.
 class _ImageAsset {
-  _ImageAsset.future(Future<Image> future) : _future = future {
+  _ImageAsset.future(Future<ui.Image> future) : _future = future {
     _future!.then((image) {
       _image = image;
       _future = null;
     });
   }
 
-  Image? get image => _image;
-  Image? _image;
+  ui.Image? get image => _image;
+  ui.Image? _image;
 
-  Future<Image>? _future;
+  Future<ui.Image>? _future;
 
-  Future<Image> retrieveAsync() => _future ?? Future.value(_image);
+  Future<ui.Image> retrieveAsync() => _future ?? Future.value(_image);
 
   /// Properly dispose of an image asset.
   void dispose() {
