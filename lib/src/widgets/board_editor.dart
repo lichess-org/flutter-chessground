9d1fd3b7354f85b3f2c77de1faa949e65a4d9b2b
Noah <78898963+HaonRekcef@users.noreply.github.com>
2025-09-19 20:49:56 +0200

    Do not update the square all the time while panning, to allow for delete on tap

--- lib/src/widgets/board_editor.dart
+++ lib/src/widgets/board_editor.dart
@@ -107,6 +107,8 @@ class ChessboardEditor extends StatefulWidget with ChessboardGeometry {
 
 class _BoardEditorState extends State<ChessboardEditor> {
   Square? draggedPieceOrigin;
+  bool _isPanning = false;
+  Square? _lastEditedSquare;
 
   @override
   Widget build(BuildContext context) {
@@ -207,9 +209,10 @@ class _BoardEditorState extends State<ChessboardEditor> {
     final board = SizedBox.square(
       dimension: widget.size,
       child: GestureDetector(
-        onTapDown: (details) => _onTouchedEvent(details.localPosition),
-        onPanStart: (details) => _onTouchedEvent(details.localPosition),
-        onPanUpdate: (details) => _onTouchedEvent(details.localPosition),
+        onTapDown: (details) => _onTapEvent(details.localPosition),
+        onPanStart: (details) => _onPanStart(details.localPosition),
+        onPanUpdate: (details) => _onPanUpdate(details.localPosition),
+        onPanEnd: (details) => _onPanEnd(),
         child: Stack(
           alignment: Alignment.topLeft,
           clipBehavior: Clip.none,
@@ -253,7 +256,7 @@ class _BoardEditorState extends State<ChessboardEditor> {
     );
   }
 
-  void _onTouchedEvent(Offset localPosition) {
+  void _onTapEvent(Offset localPosition) {
     if (widget.pointerMode == EditorPointerMode.drag) {
       return;
     }
@@ -261,6 +264,31 @@ class _BoardEditorState extends State<ChessboardEditor> {
     if (square != null) {
       widget.onEditedSquare?.call(square);
     }
+  }
+
+  void _onPanStart(Offset localPosition) {
+    if (widget.pointerMode == EditorPointerMode.drag) {
+      return;
+    }
+    _isPanning = true;
+    _lastEditedSquare = null;
+    _onPanUpdate(localPosition);
+  }
+
+  void _onPanUpdate(Offset localPosition) {
+    if (widget.pointerMode == EditorPointerMode.drag || !_isPanning) {
+      return;
+    }
+    final square = widget.offsetSquare(localPosition);
+    if (square != null && square != _lastEditedSquare) {
+      widget.onEditedSquare?.call(square);
+      _lastEditedSquare = square;
+    }
+  }
+
+  void _onPanEnd() {
+    _isPanning = false;
+    _lastEditedSquare = null;
   }
 }
 
